0e4b84bd0e9e172dc0bf97b637915eb5df5d1f94
vimrzhang <vimrzhang@tencent.com>
2025-04-10 08:23:57 +0800

    init

---
+++ Project51/Source/Project51/Project51.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Project51.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Project51, "Project51" );
+ 
---
+++ Project51/Source/Project51/Project51GameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Project51GameMode.generated.h"
+
+UCLASS(minimalapi)
+class AProject51GameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AProject51GameMode();
+};
+
+
+
